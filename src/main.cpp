e73397a816710d492bec9a0a9f17fc6d0f4853d6
Soeren <soeren@chaospott.de>
2019-02-27 19:57:19 +0100

    beschleunigung

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ const int RADIUS = 20;
 int main()
 {
     sf::Vector2f pos = {30, 30};
-    sf::Vector2f speed = {100, 100};
+    sf::Vector2f speed = {300, 300};
     sf::RenderWindow window(sf::VideoMode(LENGHT, HEIGHT), "Player");
     sf::CircleShape shape;
     shape.setRadius(RADIUS);
@@ -30,11 +30,11 @@ int main()
 
         if (shape.getPosition().x <= RADIUS && speed.x < 0 || shape.getPosition().x >= LENGHT - RADIUS && speed.x > 0)
         {
-            speed.x = -speed.x;
+            speed.x = -speed.x*1.01;
         }
         if (shape.getPosition().y <= RADIUS && speed.y < 0 || shape.getPosition().y >= HEIGHT - RADIUS && speed.y > 0)
         {
-            speed.y = -speed.y;
+            speed.y = -speed.y*1.01;
         }
 
         pos.x += speed.x * deltatime;
@@ -44,7 +44,8 @@ int main()
         window.clear();
         window.draw(shape);
         window.display();
-        std::cout << "x:" << shape.getPosition().x << "y: " << shape.getPosition().y << std::endl;
+        //std::cout << "x:" << shape.getPosition().x << "y: " << shape.getPosition().y << std::endl;
+        std::cout << speed.x << std::endl;
     }
 
     return 0;
