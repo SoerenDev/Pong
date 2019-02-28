c21f1521d16ca6dd4ba4acec4fec02af027cba29
Soeren <soeren@chaospott.de>
2019-02-28 13:40:28 +0100

    rename shape -> ball

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ int main()
     sf::Vector2f pos = {30, 30};
     sf::Vector2f speed = {300, 300};
     sf::RenderWindow window(sf::VideoMode(LENGHT, HEIGHT), "Player");
-    sf::CircleShape shape;
-    shape.setRadius(RADIUS);
-    shape.setFillColor(sf::Color::White);
+    sf::CircleShape ball;
+    ball.setRadius(RADIUS);
+    ball.setFillColor(sf::Color::White);
     sf::Clock clock;
     while (window.isOpen())
     {
@@ -28,23 +28,23 @@ int main()
             }
         }
 
-        if (shape.getPosition().x <= RADIUS && speed.x < 0 || shape.getPosition().x >= LENGHT - RADIUS && speed.x > 0)
+        if (ball.getPosition().x <= RADIUS && speed.x < 0 || ball.getPosition().x >= LENGHT - RADIUS && speed.x > 0)
         {
             speed.x = -speed.x*1.01;
         }
-        if (shape.getPosition().y <= RADIUS && speed.y < 0 || shape.getPosition().y >= HEIGHT - RADIUS && speed.y > 0)
+        if (ball.getPosition().y <= RADIUS && speed.y < 0 || ball.getPosition().y >= HEIGHT - RADIUS && speed.y > 0)
         {
             speed.y = -speed.y*1.01;
         }
 
         pos.x += speed.x * deltatime;
         pos.y += speed.y * deltatime;
-        shape.setPosition(pos);
-        shape.setOrigin(RADIUS,RADIUS);
+        ball.setPosition(pos);
+        ball.setOrigin(RADIUS,RADIUS);
         window.clear();
-        window.draw(shape);
+        window.draw(ball);
         window.display();
-        //std::cout << "x:" << shape.getPosition().x << "y: " << shape.getPosition().y << std::endl;
+        //std::cout << "x:" << ball.getPosition().x << "y: " << ball.getPosition().y << std::endl;
         std::cout << speed.x << std::endl;
     }
 
