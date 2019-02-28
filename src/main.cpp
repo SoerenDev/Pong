4054d7b5930c014c4d5a22b85062b2a0b6c669de
Soeren <soeren@chaospott.de>
2019-02-28 15:08:46 +0100

    Player added

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,25 @@
 const int LENGHT = 1000;
 const int HEIGHT = 800;
 const int SIZE = 30;
+const sf::Vector2f PLAYER_SIZE = {100, 30};
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(LENGHT, HEIGHT), "Player");
+
     sf::Vector2f pos = {30, 30};
     sf::Vector2f speed = {300, 300};
-    sf::RenderWindow window(sf::VideoMode(LENGHT, HEIGHT), "Player");
     sf::RectangleShape ball;
     ball.setSize(sf::Vector2f(SIZE,SIZE));
     ball.setFillColor(sf::Color::White);
+    
+    sf::RectangleShape player;
+    player.setSize(PLAYER_SIZE);
+    sf::Vector2f PlayerPosition = {LENGHT/2,HEIGHT*0.9};
+    player.setOrigin(PLAYER_SIZE.x/2,PLAYER_SIZE.y/2);
+
     sf::Clock clock;
+
     while (window.isOpen())
     {
         sf::Time delta = clock.getElapsedTime();
@@ -26,7 +35,17 @@ int main()
             {
                 window.close();
             }
+            
         }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && PlayerPosition.x <= LENGHT - PLAYER_SIZE.x/2) {
+                PlayerPosition.x += 700*deltatime;
+            }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && PlayerPosition.x >= PLAYER_SIZE.x/2) {
+            PlayerPosition.x -= 700*deltatime;
+        }
+
 
         if (ball.getPosition().x <= SIZE/2 && speed.x < 0 || ball.getPosition().x >= LENGHT - SIZE/2 && speed.x > 0)
         {
@@ -40,12 +59,13 @@ int main()
         pos.x += speed.x * deltatime;
         pos.y += speed.y * deltatime;
         ball.setPosition(pos);
+        player.setPosition(PlayerPosition);
         ball.setOrigin(SIZE/2,SIZE/2);
         window.clear();
         window.draw(ball);
+        window.draw(player);
         window.display();
         //std::cout << "x:" << ball.getPosition().x << "y: " << ball.getPosition().y << std::endl;
-        std::cout << speed.x << std::endl;
     }
 
     return 0;
